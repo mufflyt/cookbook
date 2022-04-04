799332d8319ff3969d82d9ef48b0e5ca260d0b71
Tyler Muffly <tyler.muffly@dhha.org>
2022-04-03 21:26:15 -0600

    Update tester.R

--- tester.R
+++ tester.R
@@ -30,9 +30,7 @@ tm_write2pdf <-
 #.libPaths("/Users/tylermuffly/.exploratory/R/4.1")
 
 # Install Packages ----
-pkgs <- (c('caret', 'readxl', 'XML', 'reshape2', 'devtools', 'purrr', 'readr', 'ggplot2', 'dplyr', 'magick', 'janitor', 'lubridate', 'hms', 'tidyr', 'stringr', 'openxlsx', 'forcats', 'RcppRoll', 'tibble', 'bit64', 'munsell', 'scales', 'rgdal', 'tidyverse', "foreach", "PASWR", "rms", "pROC", "ROCR", "nnet", "packrat", "DynNom", "caTools", "mlbench", "randomForest", "ipred", "xgboost", "Metrics", "RANN", "AppliedPredictiveModeling", "shiny", "earth", "fastAdaboost", "Boruta", "glmnet", "ggforce", "tidylog", "InformationValue", "pscl", "scoring", "DescTools", "gbm", "Hmisc", "arsenal", "pander", "moments", "leaps", "MatchIt", "car", "mice", "rpart", "beepr", "fansi", "utf8", "lmtest", "ResourceSelection", "rmarkdown", "rattle", "rmda", "funModeling", "tinytex", "caretEnsemble", "Rmisc", "corrplot", "progress", "perturbR", "vctrs", "highr", "labeling", "DataExplorer", "rsconnect", "inspectdf", "ggpubr", "tableone", "knitr", "drake", "visNetwork", "rpart.plot", "RColorBrewer", "kableExtra", "kernlab", "naivebayes", "e1071", "data.table", "skimr", "naniar", "english", "mosaic", "broom", "mltools", "tidymodels", "tidyquant", "rsample", "yardstick", "parsnip", "dials", "cowplot", "lime", "flexdashboard", "shinyjs", "shinyWidgets", "plotly", "BH", "vip", "ezknitr", "here", "corrgram", "factoextra", "parallel", "doParallel", "odbc", "RSQLite", "discrim", "doMC",  "summarytools", "remotes", "fs", "PerformanceAnalytics", "correlationfunnel", "psych", "RSelenium", "robotstxt", "corpcor", "mctest", "ppcor", "compareGroups", "fastDummies", "grpreg", "Matrix", "rstudioapi", "tune", "glue", "catboost", "beepr", "miniUI", "klaR", "moments",
-"ranger", 'R.methodsS3', 'plotROC', 'MLmetrics', 'humaniformat', 'DiagrammeR', 'DiagrammeRsvg', 'stargazer', 'coefplot', 'sjPlot', 'sjmisc', "renv", "recipes", "timeDate", "ggthemes"))
-#"ggmap", "sf",'rsvg', 
+pkgs <- (c('caret', 'readxl', 'XML', 'reshape2', 'devtools', 'tidyverse', 'magick', 'hms', 'tidyr', 'sjPlot', 'sjmisc', 'renv', 'knitr'))
 
 #install.packages(pkgs,dependencies = c("Depends", "Suggests", "Imports", "LinkingTo"), repos = "https://cloud.r-project.org")  #run this first time
 lapply(pkgs, require, character.only = TRUE)
@@ -67,19 +65,19 @@ knitr::opts_knit$set(global.par = T)
 if (packageVersion("devtools") < 1.6) {
   install.packages("devtools")
 }
-#devtools::install_github("paulhendricks/anonymizer")
+devtools::install_github("paulhendricks/anonymizer")
 library(anonymizer)
 
-#devtools::install_github("tidyverse/glue")
+devtools::install_github("tidyverse/glue")
 library(glue)
 
-#install.packages("backports")
+install.packages("backports")
 library(backports)
 
-#install.packages("psych")
+install.packages("psych")
 library(psych)
 
-#devtools::install_github("exploratory-io/exploratory_func")
+devtools::install_github("exploratory-io/exploratory_func")
 library(exploratory)
 
 remotes::install_github("r-link/corrmorant")
