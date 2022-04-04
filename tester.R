a20d8f7282b4cf89a06120dee73905e9d7cf6f5b
Tyler Muffly <tyler.muffly@dhha.org>
2022-04-03 21:07:02 -0600

    Update tester.R

--- tester.R
+++ tester.R
@@ -1,3 +1,18 @@
+
+
+set.seed(1978)   
+rm(list = setdiff(ls(), lsf.str())) #cleans all environment except functions
+invisible(gc())
+options(scipen = 3)
+options(warning.length = 2000L)
+
+
+## Default repo
+local({r <- getOption("repos")
+r["CRAN"] <- "http://cran.r-project.org" 
+options(repos=r)
+})
+
 #Bespoke functions
 tm_write2pdf <- 
   function(object, filename) {  
@@ -9,3 +24,40 @@ tm_write2pdf <-
                        quiet = TRUE) # passed to rmarkdown::render
     #pander::openFileInOS((paste0(filename, ".pdf"))) #sweet
   }
+
+
+# Set libPaths ----
+#.libPaths("/Users/tylermuffly/.exploratory/R/4.1")
+
+# Install Packages ----
+pkgs <- (c('caret', 'readxl', 'XML', 'reshape2', 'devtools', 'purrr', 'readr', 'ggplot2', 'dplyr', 'magick', 'janitor', 'lubridate', 'hms', 'tidyr', 'stringr', 'openxlsx', 'forcats', 'RcppRoll', 'tibble', 'bit64', 'munsell', 'scales', 'rgdal', 'tidyverse', "foreach", "PASWR", "rms", "pROC", "ROCR", "nnet", "packrat", "DynNom", "caTools", "mlbench", "randomForest", "ipred", "xgboost", "Metrics", "RANN", "AppliedPredictiveModeling", "shiny", "earth", "fastAdaboost", "Boruta", "glmnet", "ggforce", "tidylog", "InformationValue", "pscl", "scoring", "DescTools", "gbm", "Hmisc", "arsenal", "pander", "moments", "leaps", "MatchIt", "car", "mice", "rpart", "beepr", "fansi", "utf8", "lmtest", "ResourceSelection", "rmarkdown", "rattle", "rmda", "funModeling", "tinytex", "caretEnsemble", "Rmisc", "corrplot", "progress", "perturbR", "vctrs", "highr", "labeling", "DataExplorer", "rsconnect", "inspectdf", "ggpubr", "tableone", "knitr", "drake", "visNetwork", "rpart.plot", "RColorBrewer", "kableExtra", "kernlab", "naivebayes", "e1071", "data.table", "skimr", "naniar", "english", "mosaic", "broom", "mltools", "tidymodels", "tidyquant", "rsample", "yardstick", "parsnip", "dials", "cowplot", "lime", "flexdashboard", "shinyjs", "shinyWidgets", "plotly", "BH", "vip", "ezknitr", "here", "corrgram", "factoextra", "parallel", "doParallel", "odbc", "RSQLite", "discrim", "doMC",  "summarytools", "remotes", "fs", "PerformanceAnalytics", "correlationfunnel", "psych", "RSelenium", "robotstxt", "corpcor", "mctest", "ppcor", "compareGroups", "fastDummies", "grpreg", "Matrix", "rstudioapi", "tune", "glue", "catboost", "beepr", "miniUI", "klaR", "moments",
+"ranger", 'R.methodsS3', 'plotROC', 'MLmetrics', 'humaniformat', 'DiagrammeR', 'DiagrammeRsvg', 'stargazer', 'coefplot', 'sjPlot', 'sjmisc', "renv", "recipes", "timeDate", "ggthemes"))
+#"ggmap", "sf",'rsvg', 
+
+#install.packages(pkgs,dependencies = c("Depends", "Suggests", "Imports", "LinkingTo"), repos = "https://cloud.r-project.org")  #run this first time
+lapply(pkgs, require, character.only = TRUE)
+rm(pkgs)
+
+#Renv stuff
+renv::settings$snapshot.type("all")
+#renv::snapshot()
+
+# install.packages("remotes")
+#remotes::install_github("lmullen/genderdata")
+#install.packages("genderdata", repos = "http://packages.ropensci.org")
+options(download.file.method = "libcurl")
+#devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
+#devtools::install_url('https://github.com/catboost/catboost/releases/download/v0.23.2/catboost-R-Darwin-0.23.2.tgz', INSTALL_opts = c("--no-multiarch"))
+
+
+# Knitr Options ----
+knitr::opts_chunk$set(comment = "#>", echo = FALSE, fig.width = 6, message = FALSE, warning = FALSE, error = FALSE, strip.white = TRUE, include = FALSE)
+
+# Set code width to 60 to contain within PDF margins
+knitr::opts_chunk$set(tidy = F, tidy.opts = list(width.cutoff = 60))
+
+# Set all figures to be centered
+knitr::opts_chunk$set(fig.align = "center")
+
+# Set and preserve par(mfcol()) between chunks (calls to it can be hidden)
+knitr::opts_knit$set(global.par = T)
